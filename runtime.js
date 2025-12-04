59dfd3ad199439307a4f42afe0a682e9d65088e2
Kasper Isager Dalsgarð <kasperisager@hey.com>
2025-12-04 14:59:54 +0100

    Add UIKit prebuilds

--- runtime.js
+++ runtime.js
@@ -1,9 +1,11 @@
 const AppKit = require('bare-app-kit/runtime')
+const UIKit = require('bare-ui-kit/runtime')
 const GTK = require('bare-gtk/runtime')
 const WinUI = require('bare-win-ui/runtime')
 
 exports.prebuilds = {
   ...AppKit.prebuilds,
+  ...UIKit.prebuilds,
   ...GTK.prebuilds,
   ...WinUI.prebuilds
 }
