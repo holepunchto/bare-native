04cc18c57f12505d51628b73abf2904c06d77af9
Kasper Isager Dalsgarð <kasperisager@hey.com>
2025-12-10 09:57:07 +0100

    Add `bare-ndk`

--- runtime.js
+++ runtime.js
@@ -1,11 +1,13 @@
 const AppKit = require('bare-app-kit/runtime')
 const UIKit = require('bare-ui-kit/runtime')
 const GTK = require('bare-gtk/runtime')
+const NDK = require('bare-ndk/runtime')
 const WinUI = require('bare-win-ui/runtime')
 
 exports.prebuilds = {
   ...AppKit.prebuilds,
   ...UIKit.prebuilds,
   ...GTK.prebuilds,
+  ...NDK.prebuilds,
   ...WinUI.prebuilds
 }
